5a60a06a205739748b1ba13fff587ac4824e6563
Vedad <veda0039gt@gmail.com>
2026-09-15 19:15:48 +0200

    Changes

--- PRIMITIVE-DATA-STUCTURES/Program.cs
+++ PRIMITIVE-DATA-STUCTURES/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = [1, 8, 7, 56, 90];
+int max = numbers[0];
+
+foreach (int number in numbers)
+{
+    if (number > max)
+    {
+        max = number;
+    }
+}
+
+Console.WriteLine(max);
