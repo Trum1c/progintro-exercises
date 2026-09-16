6c7d5f37c5db3569a57cc406583482dc71df4648
Vedad <veda0039gt@gmail.com>
2026-09-16 19:04:37 +0200

    Changes

--- PRIMITIVE-DATA-STUCTURES/Program.cs
+++ PRIMITIVE-DATA-STUCTURES/Program.cs
@@ -16,33 +16,40 @@ Console.WriteLine(max);
 //7.12: Calendar
 int[] months = [31,28,31,30,31,30,31,31,30,31,30,31];
 int[] monthsLeap = [31,29,31,30,31,30,31,31,30,31,30,31];
-int [] pointer;
+int[] pointer;
 for (int year = 2000 ; year<=2020 ; year++){
     if (year % 4 == 0)
     {
         pointer = monthsLeap;
     }
     pointer = months;
-    Console.Write("Days in the months of " + year + ": ");
+    Console.WriteLine("Days in the months of " + year + ": ");
     foreach (int num in pointer){
-        Console.Write(num + " ");
+        Console.WriteLine(num + " ");
     }
 }
 
 //7.15 person
-
-// class person{
+// ** class person{
 //    public int = 18;
 //    public string = "Anton";
 //    bool alive = true;
-//} 
+//} ** 
 
 //7.16 Direction
-Console.WriteLine(direction.south);
+//Console.WriteLine(direction.south);
 
-enum direction{
-    north,
-    south,
-    east,
-    west,
+//enum direction{
+ //   north,
+ //   south,
+  //  east,
+   // west,
+//}
+
+// 7.14: Sūdoku Checker
+
+// 7.8 Areas of Circles
+int[] radius = [1,3,5];
+for (int x=0 ; x<radius.Length ; x++){
+    Console.WriteLine("The area of radius: " + radius[x] + " is: " + 3.14f * radius[x]*radius[x]);
 }
