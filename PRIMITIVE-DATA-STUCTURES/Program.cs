1f14717586adaf3eb9492c0b496cc0d7dbc5ee8f
Vedad <veda0039gt@gmail.com>
2026-09-16 15:52:53 +0200

    changes

--- PRIMITIVE-DATA-STUCTURES/Program.cs
+++ PRIMITIVE-DATA-STUCTURES/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] numbers = [1, 8, 7, 56, 90];
+﻿// 7.4: Largest in Array
+int[] numbers = [1, 8, 7, 56, 90];
 int max = numbers[0];
 
 foreach (int number in numbers)
@@ -10,3 +11,38 @@ foreach (int number in numbers)
 }
 
 Console.WriteLine(max);
+
+
+//7.12: Calendar
+int[] months = [31,28,31,30,31,30,31,31,30,31,30,31];
+int[] monthsLeap = [31,29,31,30,31,30,31,31,30,31,30,31];
+int [] pointer;
+for (int year = 2000 ; year<=2020 ; year++){
+    if (year % 4 == 0)
+    {
+        pointer = monthsLeap;
+    }
+    pointer = months;
+    Console.Write("Days in the months of " + year + ": ");
+    foreach (int num in pointer){
+        Console.Write(num + " ");
+    }
+}
+
+//7.15 person
+
+// class person{
+//    public int = 18;
+//    public string = "Anton";
+//    bool alive = true;
+//} 
+
+//7.16 Direction
+Console.WriteLine(direction.south);
+
+enum direction{
+    north,
+    south,
+    east,
+    west,
+}
